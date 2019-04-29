2bfee89932711e18ab9a671f383273d5545610f1
Brandon Roesler <brandonroesler@Brandons-MacBook-Pro.local>
2019-04-29 09:23:39 -0500

    adding all the files so far

---
+++ js/main.js
@@ -0,0 +1,71 @@
+/*----- constants -----*/
+const PLAYERS = {
+    '1': 'Blue',
+    '-1': 'Black',
+    '0': 'white'
+};
+
+/*----- app's state (variables) -----*/
+var board, winner, turn;
+
+/*----- cached element references -----*/ 
+const msgEl = document.getElementById('msg');
+
+/*----- event listeners -----*/ 
+
+document.getElementById('squares')
+  .addEventListener('click', handleClick);
+
+/*----- functions -----*/
+init();
+
+function handleClick(evt) {
+    const marker = evt.target;
+    const colIdx = parseInt(marker.id.replace('c', ''));
+    if (isNaN(colIdx) || winner) return;
+    const rowIdx = board[colIdx].indexOf(0);
+    if (rowIdx === -1) return;
+    board[colIdx][rowIdx] = turn;
+    /* 
+    winner = getWinner();
+    */
+    turn *= -1;
+    render();
+}
+
+function render() {
+// Display the board
+
+board.forEach(function(colArr, colIdx) {
+    // Update col markers
+    /*const marker = document.getElementById(`col${colIdx}`);
+    marker.style.borderTopColor = colArr.includes(0) ? 'lightgrey' : 'white';*/
+    colArr.forEach(function(cell, rowIdx) {
+    // Access the correct div in the section
+    const div = document.getElementById(`c${colIdx}r${rowIdx}`);
+    div.style.backgroundColor = PLAYERS[cell];
+    });
+});
+
+// Display message
+if (winner) {
+    if (winner === 'T') {
+    msgEl.textContent = "It's a Tie!";
+    } else {
+    /* msgEl.innerHTML = `<span style="color:${COLORS[winner]}">${COLORS[winner].toUpperCase()}</span> Wins!`;*/
+    }
+} else {
+    msgEl.innerHTML = `<span style="color:${PLAYERS[turn]}">${PLAYERS[turn].toUpperCase()}</span>'s Turn`;
+}
+}
+
+function init() {
+board = [
+    [0, 0, 0],  // column 1
+    [0, 0, 0],  // column 2
+    [0, 0, 0],  // column 3
+];
+winner = null;
+turn = 1;
+render();
+}
